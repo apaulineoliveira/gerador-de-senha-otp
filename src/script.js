66fcec83df85740a3fa06a313eae1e583e9f7387
apaulineoliveira <apaulineoliveira@gmail.com>
2025-03-21 00:02:06 -0300

    Refactor (src/script.js) : Armazena a função setTimeout em uma cosntante

--- src/script.js
+++ src/script.js
@@ -1,46 +1,42 @@
-function generateOTP() {
-  let digits = "0123456789";
-  let OTP = '';
-
-
-  for (let i = 0; i < 6; i++) {
-      OTP += digits[Math.floor(Math.random() * 10)];
-  }
-
-
-  let textarea = document.querySelector('#textarea');
-  textarea.value = OTP;
-
-
-  const STARTING_MINUTES = 10;
-  let time = STARTING_MINUTES * 60; 
-
-  const COUNT_DOWN_EL = document.getElementById('countdown');
-
-  
-  setInterval(updateCountdown, 1000);
-
-  function updateCountdown() {
-      const minutes = Math.floor(time / 60);
-      let seconds = time % 60;
-      seconds = seconds < 10 ? '0' + seconds : seconds;
-
-    
-      COUNT_DOWN_EL.innerHTML = `${minutes}:${seconds}`;
-      
-      time--;
-
-     
-      if (time < 0) {
-          clearInterval();
-          COUNT_DOWN_EL.innerHTML = "00:00";
-      }
-  }
+function gerarOTP() {
+    const numeros = "0123456789"; 
+    let codigoOTP = gerarCodigoAleatorio(numeros, 6); 
+    document.querySelector('#campoTexto').value = codigoOTP;
+    iniciarContagemRegressiva(10); // Inicia a contagem de 10 minutos
 }
 
-const atualizaPagina = () => {
-	window.location.reload(1);
-};
+function gerarCodigoAleatorio(alfabeto, comprimento) {
+    let codigo = '';
+    for (let i = 0; i < comprimento; i++) {
+        codigo += alfabeto[Math.floor(Math.random() * alfabeto.length)];
+    }
+    return codigo;
+}
 
-setTimeout(atualizaPagina, 600000);
 
+function iniciarContagemRegressiva(minutos) {
+    const tempoTotal = minutos * 60;
+    let tempoRestante = tempoTotal; 
+    const elementoContador = document.getElementById('contador'); 
+
+    const intervalo = setInterval(() => {
+        const { minutos, segundos } = calcularTempoRestante(tempoRestante);
+        elementoContador.innerHTML = `${minutos}:${segundos}`;
+        tempoRestante--;
+
+        if (tempoRestante < 0) {
+            clearInterval(intervalo);
+            elementoContador.innerHTML = "00:00";
+        }
+    }, 1000);
+}
+
+function calcularTempoRestante(segundos) {
+    const minutos = Math.floor(segundos / 60);
+    const segundosRestantes = segundos % 60;
+    const segundosFormatados = segundosRestantes < 10 ? `0${segundosRestantes}` : segundosRestantes;
+    return { minutos, segundos: segundosFormatados };
+}
+
+const recarregarPagina = () => window.location.reload();
+setTimeout(recarregarPagina, 600000);
